66e3f4c4ab2331052c304085f018f5ad8c6eae5c
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-06-09 14:36:48 +0300

    Add files via upload

---
+++ src/using_css_selectors.py
@@ -0,0 +1,31 @@
+import requests
+from pprint import pprint
+
+payload = {
+    "source": "universal",
+    "url": "https://books.toscrape.com/catalogue/page-1.html",
+    "parse": True,
+    "parsing_instructions": {
+        "titles": {
+            "_fns": [
+                {
+                    "_fn": "css",
+                    "_args": ["h3 > [title]"]
+                },
+                {
+                    "_fn": "element_text"
+                }
+            ]
+        }
+    }
+}
+
+
+response = requests.request(
+    "POST",
+    "https://realtime.oxylabs.io/v1/queries",
+    auth=("USERNAME", "PASSWORD"),
+    json=payload
+)
+
+pprint(response.json())
